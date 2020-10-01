64e55a879507d082fdd9ca37ffe1a53164b97e5d
Kyoko OZAKI <>
2020-10-01 23:42:02 +0900

    Lambda Function を作成

--- bin/cdk-rest-api-sample.ts
+++ bin/cdk-rest-api-sample.ts
@@ -4,4 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { RestApiStack } from '../lib/rest-api-stack';
 
 const app = new cdk.App();
-new RestApiStack(app, 'RestApiStack');
+new RestApiStack(app, 'RestApiStack', {
+    env: {
+        region: 'ap-northeast-1',
+    }
+});
--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import { Function, Runtime, Code } from '@aws-cdk/aws-lambda';
+import { Duration } from '@aws-cdk/core';
 
 export class RestApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.createLambdaFunction();
+  }
 
-    // The code that defines your stack goes here
+  private createLambdaFunction() {
+    return new Function(this, 'LambdaHandler', {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.fromAsset('./dist'),
+      handler: 'index.handler',
+      timeout: Duration.minutes(1),
+    });
   }
 }
